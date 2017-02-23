3bf2a22cb1e85dfbdd868b5ee6426a09c5013208
K. Scott Allen <scott@OdeToCode.com>
2017-02-23 04:32:05 -0500

    added identity sample

--- 2015/AtTheMovies/src/AtTheMovies/Controllers/HomeController.cs
+++ 2015/AtTheMovies/src/AtTheMovies/Controllers/HomeController.cs
@@ -27,33 +27,6 @@ namespace AtTheMovies.Controllers
         }
 
 
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Create(Movie newMovie)
-        {
-            if (ModelState.IsValid)
-            {
-                _movies.Add(newMovie);
-                _movies.Commit();
-                return RedirectToAction("Details", new {id = newMovie.Id});
-            }
-            return View(newMovie);
-        }
-
-        [Route("{id}")]
-        public IActionResult Details(int id)
-        {
-            var model = _movies.GetById(id);
-            if (model == null)
-            {
-                return NotFound();
-            }
-            return View(model);
-        }
+      
     }
 }
---
+++ 2015/AtTheMovies/src/AtTheMovies/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using AtTheMovies.Models;
+using AtTheMovies.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtTheMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly IMovieDataStore _movies;
+
+        public MoviesController(IMovieDataStore movies)
+        {
+            _movies = movies;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Movie newMovie)
+        {
+            if (ModelState.IsValid)
+            {
+                _movies.Add(newMovie);
+                _movies.Commit();
+                return RedirectToAction("Details", new { id = newMovie.Id });
+            }
+            return View(newMovie);
+        }
+
+        [Route("{id}")]
+        public IActionResult Details(int id)
+        {
+            var model = _movies.GetById(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return View(model);
+        }
+    }
+}
